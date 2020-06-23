356ec6ce5285d583106a644e73abf95620d6ad8b
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-06-23 20:58:44 +0500

    Custom pages implemented, such as Full-Width

---
+++ template-full-width.php
@@ -0,0 +1,31 @@
+<?php
+// Template Name: Full Width
+// Template Post Type: post, page
+?>
+
+<?php get_header(); ?>
+
+<!-- Primary Content -->
+<div id="primary" class="content-area extended">
+
+  <main id="main" class="site-main" role="main">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <!-- Display Content -->
+        <?php get_template_part('template-parts/content', 'page'); ?>
+
+    <?php endwhile; else : ?>
+
+        <!-- No Content -->
+        <?php get_template_part('template-parts/content', 'none'); ?>
+
+    <?php endif; ?>
+
+    <p>Template: template-full-width.php</p>
+
+    </main>
+
+</div>
+
+<?php get_footer(); ?>
