5ddbb002b0144a5dbcf0a6f12e8d27df08e15dbf
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-06-23 21:10:15 +0500

    404.php implemented

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<!-- Primary Content -->
+<div id="primary" class="content-area extended">
+
+  <main id="main" class="site-main" role="main">
+
+    <!-- No Content -->
+    <?php get_template_part('template-parts/content', 'none'); ?>
+
+    <p>Template: 404.php</p>
+
+    </main>
+
+</div>
+
+<?php get_footer(); ?>
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -2,12 +2,18 @@
 
   <!-- 404 Title -->
   <header class="entry-header">
-    <h1><?php esc_html_e( '404', 'wphierarchy' ); ?></h1>
+    <h1><?php esc_html_e( '404 - Page Not Found', 'wphierarchy' ); ?></h1>
   </header>
 
   <!-- 404 - Description -->
   <div class="entry-content">
+
+    <!-- 404 Message -->
     <p><?php esc_html_e( 'Sorry! No content found.', 'wphierarchy' ); ?></p>
+
+    <!-- Search Form -->
+    <p><?php echo get_search_form(); ?></p>
+
   </div>
 
 </article>
